5f804f9ca847981bb5ca91d6673a26e11c4afcee
root <root@ip-172-31-33-180.ec2.internal>
2020-09-11 17:40:14 +0000

    content changed

--- changeset.sql
+++ changeset.sql
@@ -1,7 +1,8 @@
 --liquibase formatted sql
 --changeset devopsadmin:1
-create table test1 ( 
+create table rakesh1 ( 
 id int primary key,
-name varchar(255)
+name varchar(255),
+address varchar(255)
 );
---rollback drop table test1;
+--rollback drop table rakesh1;
