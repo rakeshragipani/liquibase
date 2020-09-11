946c62b5de1c4449656dfd8917bd6a217af2a4ca
root <root@ip-172-31-33-180.ec2.internal>
2020-09-11 18:10:46 +0000

    files modified

--- dir2/changeset.sql
+++ dir2/changeset.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeset devopsadmin:1
-create table release ( 
+create table release2 ( 
 id int primary key,
 name varchar(255),
 address varchar(255)
 );
---rollback drop table release;
+--rollback drop table release2;
