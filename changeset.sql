3963f6aaf859379b0da794cf099214cb52ebf829
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-14 22:02:19 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeset devopsadmin:1
-create table master ( 
+create table develop ( 
 id int primary key,
 name varchar(255),
 address varchar(255)
 );
---rollback drop table master;
+--rollback drop table develop;
