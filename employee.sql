b782cbad4f06eca710c5d58971f6d8c51931b39e
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-17 15:33:15 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,4 +1,8 @@
 --liquibase formatted sql
 --changeset devopsadmin:3
-alter table employee add column phonenummber int;
---rollback alter table employee drop column phonenummber;
+create table employee ( 
+id int primary key,
+name varchar(255),
+address varchar(255)
+);
+--rollback drop table employee;
