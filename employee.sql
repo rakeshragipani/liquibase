48546298afbc258f90a8457350dcafccc992d808
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-18 15:24:54 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -2,3 +2,4 @@
 --changeset devopsadmin:3
 alter table employee add column phonenummber int;
 --rollback alter table employee drop column phonenummber;
+--validCheckSum: 8:85bdca82ea9b3aa285fbeed98c5bf861
