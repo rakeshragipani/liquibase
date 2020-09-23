0ba4a60d829b3fdaa3180f9ec33cf8f3eb07f2d2
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-24 00:46:00 +0530

    Update changeset.sql

--- dir1/changeset.sql
+++ dir1/changeset.sql
@@ -6,3 +6,6 @@ name varchar(255),
 address varchar(255)
 );
 --rollback drop table develop1;
+
+alter table develop1 add column phonenummber int;
+--rollback alter table develop1 drop column phonenummber;
