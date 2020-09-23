d8ae0559accd2925a4109d1c89b96f0546a24f55
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-24 00:32:02 +0530

    Delete employee.sql

--- employee.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset devopsadmin:3
-alter table employee add column phonenummber int;
---rollback alter table employee drop column phonenummber;
---validCheckSum: 8:85bdca82ea9b3aa285fbeed98c5bf861
