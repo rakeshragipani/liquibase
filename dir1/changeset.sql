36d00305f4190f9adc372cb49d062838a8e4cc69
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-09-24 00:56:38 +0530

    Update changeset.sql

--- dir1/changeset.sql
+++ dir1/changeset.sql
@@ -1,5 +1,12 @@
 --liquibase formatted sql
 --changeset devopsadmin:1
+create table develop1 ( 
+id int primary key,
+name varchar(255),
+address varchar(255)
+);
+--rollback drop table develop1;
+--changeset devopsadmin:2
 alter table develop1 add column phonenummber int;
 --rollback alter table develop1 drop column phonenummber;
 --validCheckSum: 8:f709b6c46aeb1a461456f58550c6221d
